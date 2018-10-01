252391c0a5fc704fba61f7985a51a6045d64c658
Lauren Anfenson <laurenanfenson@gmail.com>
2018-10-01 18:59:46 -0400

    bones for the ALUs

--- alu.v
+++ alu.v
@@ -1,4 +1,46 @@
-module ALU 
+
+module ALU_first
+(
+output out,
+output carryout,
+input A,
+input B,
+input SLT,
+input S0,
+input S1
+);
+  //code
+endmodule
+
+module ALU_last
+(
+  output out,
+  output overflow,
+  output carryout,
+  output SLT,
+  input A,
+  input B,
+  input carryin,
+  input S0,
+  input S1
+);
+  //this is the plan
+endmodule
+
+module ALU_1ALU_1bit
+(
+  output out,
+  output carryout,
+  input A,
+  input B,
+  input carryin,
+  input S0,
+  input S1
+);
+  //another plan for you chief
+endmodule
+
+module ALU
 (
 output[31:0]  result,
 output        carryout,
